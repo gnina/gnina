0a4640c65fef6692f2bf66755b4846fbcded53b5
dkoes <dkoes@pitt.edu>
2015-03-03 00:14:47 -0500

    json support

--- src/server/MinimizationQuery.h
+++ src/server/MinimizationQuery.h
@@ -168,9 +168,10 @@ public:
 	//all of the result/output functions can be called while an asynchronous
 	//query is running
 
-	//return all current results
-
+	//return all current results summarized
 	void outputData(const MinimizationFilters& dp, ostream& out);
+	void outputJSONData(const MinimizationFilters& dp, ostream& out);
+
 	//write out all results in sdf.gz format
 	void outputMols(const MinimizationFilters& dp, ostream& out);
 	//output single mol in sdf format; referenced using current position in processed results array
