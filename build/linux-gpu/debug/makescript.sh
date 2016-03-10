6238ad608cccf265b41abddc0e1d0a5ee739364e
Jocelyn Sunseri <jss97@pitt.edu>
2016-03-10 14:32:53 -0500

    Modified makescript to copy binary to test.

--- build/linux-gpu/debug/makescript.sh
+++ build/linux-gpu/debug/makescript.sh
@@ -8,3 +8,5 @@ make -j8 NVOPTS='-O0 -g -G -lineinfo' BASE=/usr CUDA_INCLUDE=/opt/cuda/include O
 else
 make -j8 NVOPTS='-O0 -g -G -lineinfo' BASE=/usr CUDA_INCLUDE=/usr/local/cuda/include OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  NVCC=/usr/local/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/usr/local/cuda/lib64'
 fi
+
+cp smina.gpu ../../../test/
