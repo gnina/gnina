54109f2441495518999eebebff1217ecbca8e19e
Jocelyn Sunseri <jss97@pitt.edu>
2016-05-08 06:33:59 -0400

    Profiling. Pushing.

--- build/linux-gpu/release/makescript.sh
+++ build/linux-gpu/release/makescript.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
+# make -j8 -O BASE=/usr CUDA_INCLUDE=/opt/cuda/include OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  NVCC=/opt/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/opt/cuda/lib64'
+
 if [ "$(hostname)" = "t410" ]
 then
-    make -j8 \
-         C_OPTIONS='-O3 -DNDEBUG -g'
-         BASE=/usr \
-         CUDA_INCLUDE=/opt/cuda/include \
-         OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  \
-         NVCC=/opt/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/opt/cuda/lib64'
+    make -j4 BASE=/usr CUDA_INCLUDE=/opt/cuda/include OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  NVCC=/opt/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/opt/cuda/lib64'
 else
-    make -j8 \
-         C_OPTIONS='-O3 -DNDEBUG -g' \
-         BASE=/usr \
-         CUDA_INCLUDE=/usr/local/cuda/include \
-         OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  \
-         NVCC=/usr/local/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/usr/local/cuda/lib64'
+    make -j8 BASE=/usr CUDA_INCLUDE=/usr/local/cuda/include OPENBABEL_INCLUDE=/usr/include/openbabel-2.0  NVCC=/usr/local/cuda/bin/nvcc LDFLAGS='-L/usr/lib -L. -L/usr/local/lib -L/usr/local/cuda/lib64'\
+         2>&1 | sed -r "s/\\(([0-9]+)\\)/:\\1/g" 1>&2;
 fi
 
 cp smina.gpu ../../../test/
--- src/lib/model.cpp
+++ src/lib/model.cpp
@@ -993,8 +993,8 @@ fl model::eval_deriv(const precalculate& p, const igrid& ig, const vec& v,
 	set_gpu(c);
     cudaDeviceSynchronize();
 	fl e = ig.eval_deriv(*this, v[1], user_grid); // sets minus_forces, except inflex
-	e += eval_interacting_pairs_deriv(p, v[2], other_pairs, coords,
-			minus_forces); // adds to minus_forces
+	/* e += eval_interacting_pairs_deriv(p, v[2], other_pairs, coords, */
+	/* 		minus_forces); // adds to minus_forces */
 	VINA_FOR_IN(i, ligands)
 		e += eval_interacting_pairs_deriv(p, v[0], ligands[i].pairs, coords,
 				minus_forces); // adds to minus_forces
