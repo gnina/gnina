f02b72a11300952e16be17cb2635b324916dc22f
David Koes <dkoes@pitt.edu>
2016-05-16 21:18:36 -0400

    fix sharedmemory exclusive scan
    we're now better than 5X faster than the cpu including copy and setup
    times (the main improvement was removing an assert)

--- src/lib/gridoptions.h
+++ src/lib/gridoptions.h
@@ -27,7 +27,8 @@ struct gridoptions
 	bool binary;
 	bool gpu;
 	gridoptions() :
-			dim(24), res(0.5), verbosity(1), seed((int)time(NULL)),
+		//a default dimension of 23.5 yields 48x48x48 gridpoints
+			dim(23.5), res(0.5), verbosity(1), seed((int)time(NULL)),
 			randrotate(false), randtranslate(0.0),
 			help(false), version(false), timeit(false), outmap(false), binary(false), gpu(false)
 	{
--- src/lib/nngridder_gpu.cu
+++ src/lib/nngridder_gpu.cu
@@ -1,4 +1,5 @@
 #include "nngridder.h"
+#include <boost/timer/timer.hpp>
 
 // GPU routines for nngridder
 
@@ -8,33 +9,37 @@
 #define LOG2_WARP_SIZE 5U
 #define WARP_SIZE (1U << LOG2_WARP_SIZE)
 
-__shared__ volatile uint scanScratch[THREADSPERBLOCK * 2];
-__shared__ uint scanOutput[2][THREADSPERBLOCK];
+__shared__ uint scanOutput[THREADSPERBLOCK];
 __shared__ uint atomIndices[THREADSPERBLOCK];
 __shared__ uint atomMask[THREADSPERBLOCK];
+__shared__ uint scanScratch[THREADSPERBLOCK * 2];
 
 
+
+/*
 //do a scan and return ptr to result (could be either place in double-buffer)
+__shared__ uint scanBuffer[2][THREADSPERBLOCK];
 __device__ uint* scan(int thid)
 {
 	int pout = 0, pin = 1;
 // load input into shared memory.
 // This is exclusive scan, so shift right by one and set first elt to 0
-	scanOutput[0][thid] = (thid > 0) ? atomMask[thid - 1] : 0;
-	scanOutput[1][thid] = 0;
+	scanBuffer[0][thid] = (thid > 0) ? atomMask[thid - 1] : 0;
+	scanBuffer[1][thid] = 0;
 	__syncthreads();
 	
 	for(int offset = 1; offset < THREADSPERBLOCK; offset *= 2){
 		pout = 1 - pout; // swap double buffer indices
 		pin = 1 - pin;
 		if(thid >= offset)
-			scanOutput[pout][thid] = scanOutput[pin][thid] + scanOutput[pin][thid - offset];
+			scanBuffer[pout][thid] = scanBuffer[pin][thid] + scanBuffer[pin][thid - offset];
 		else
-			scanOutput[pout][thid] = scanOutput[pin][thid];
+			scanBuffer[pout][thid] = scanBuffer[pin][thid];
 		__syncthreads();
 	}
-	return scanOutput[pout];
+	return scanBuffer[pout];
 }
+*/
 
 //Almost the same as naive scan1Inclusive, but doesn't need __syncthreads()
 //assuming size <= WARP_SIZE
@@ -46,9 +51,9 @@ warpScanInclusive(int threadIndex, uint idata, volatile uint *s_Data, uint size)
 	s_Data[pos] = idata;
 
 	for(uint offset = 1; offset < size; offset <<= 1)
-	s_Data[pos] += s_Data[pos - offset];
+		s_Data[pos] += s_Data[pos - offset];
 
-	return s_Data[pos];
+	return s_Data[pos]; 
 }
 
 inline __device__ uint
@@ -57,35 +62,36 @@ warpScanExclusive(int threadIndex, uint idata, volatile uint *sScratch, uint siz
 }
 
 __inline__ __device__ void
-sharedMemExclusiveScan(int threadIndex, uint* sInput, uint* sOutput, volatile uint* sScratch, uint size)
+sharedMemExclusiveScan(int threadIndex, uint* sInput, uint* sOutput)
 {
 	uint idata = sInput[threadIndex];
-
 	//Bottom-level inclusive warp scan
-	uint warpResult = warpScanInclusive(threadIndex, idata, sScratch, WARP_SIZE);
+	uint warpResult = warpScanInclusive(threadIndex, idata, scanScratch, WARP_SIZE);
+
 
 	// Save top elements of each warp for exclusive warp scan sync
 	// to wait for warp scans to complete (because s_Data is being
 	// overwritten)
 	__syncthreads();
-
-	if ( (threadIndex & (WARP_SIZE - 1)) == (WARP_SIZE - 1) )
-	sScratch[threadIndex >> LOG2_WARP_SIZE] = warpResult;
+	
+	if ( (threadIndex & (WARP_SIZE - 1)) == (WARP_SIZE - 1) ) {
+		scanScratch[threadIndex >> LOG2_WARP_SIZE] = warpResult;
+	}
 
 	// wait for warp scans to complete
 	__syncthreads();
 
 	if ( threadIndex < (THREADSPERBLOCK / WARP_SIZE)){
 		// grab top warp elements
-		uint val = sScratch[threadIndex];
+		uint val = scanScratch[threadIndex];
 		// calculate exclusive scan and write back to shared memory
-		sScratch[threadIndex] = warpScanExclusive(threadIndex, val, sScratch, size >> LOG2_WARP_SIZE);
+		scanScratch[threadIndex] = warpScanExclusive(threadIndex, val, scanScratch, THREADSPERBLOCK >> LOG2_WARP_SIZE);
 	}
 
 	//return updated warp scans with exclusive scan results
 	__syncthreads();
 
-	sOutput[threadIndex] = warpResult + sScratch[threadIndex >> LOG2_WARP_SIZE] - idata;
+	sOutput[threadIndex] = warpResult + scanScratch[threadIndex >> LOG2_WARP_SIZE] - idata;
 }
 
 //return squared distance between pt and (x,y,z)
@@ -110,7 +116,7 @@ template<bool Binary> __device__ void set_atoms(float3 origin, int dim, float re
 	unsigned zi = threadIdx.z + blockIdx.z*blockDim.z;
 
 	if(xi >= dim || yi >= dim || zi >= dim)
-	return;//bail if we're off-grid, this should not be common
+		return;//bail if we're off-grid, this should not be common
 
 	unsigned gsize = dim*dim*dim;
 	//compute x,y,z coordinate of grid point
@@ -180,7 +186,8 @@ template<bool Binary> __device__ void set_atoms(float3 origin, int dim, float re
 //return 1 if atom potentially overlaps block, 0 otherwise
 __device__
 unsigned atomOverlapsBlock(unsigned aindex,float3 origin,float resolution,
-		float3 *coords,float *radii,short *gridindex,float rmult){
+		float3 *coords,float *radii,short *gridindex,float rmult)
+{
 
 	if(gridindex[aindex] < 0)
 		return 0; //hydrogen
@@ -221,20 +228,15 @@ unsigned atomOverlapsBlock(unsigned aindex,float3 origin,float resolution,
 }
 
 __device__
-bool scanValid(unsigned idx,uint *scanresult){
-	if(idx == 0){
-		for(uint i = 1; i < THREADSPERBLOCK; i++){
-			//printf("%d %d %d\n",scanOutput[i-1],scanOutput[i],atomMask[i]); 
-			assert(scanresult[i] >= scanresult[i - 1]);
-			if(scanresult[i] > scanresult[i - 1]){
-				if(!atomMask[i-1]) {
-					printf("ERROR %d,%d,%d,%d %d %d %d\n",atomMask[i-2],atomMask[i-1],atomMask[i],atomMask[i+1],scanresult[i-1],scanresult[i],i);
-					
-				}
-				assert(atomMask[i - 1]);
-			}
+bool scanValid(unsigned idx,uint *scanresult)
+{
+	for(uint i = 1; i < THREADSPERBLOCK; i++){
+		assert(scanresult[i] >= scanresult[i - 1]);
+		if(scanresult[i] > scanresult[i - 1]){
+			assert(atomMask[i - 1]);
 		}
 	}
+	
 	return true;
 }
 
@@ -248,16 +250,12 @@ bool scanValid(unsigned idx,uint *scanresult){
 //grids are the output and are assumed to be zeroed
 template<bool Binary> __global__ void gpu_grid_set(float3 origin, int dim, float resolution, float rmult, int n, float3 *coords, short *gridindex, float *radii, float *grids)
 {
-	//PLAN: the below needs to be put into a function that takes an array of atom indices (with its length) that's in shared memory
-	//then we can write a loop that, in shared memsized chunks (size of arrays should be identical to threads per block)
-	//"assignCircles" the atoms, that is, in parallel evaluate if atom possibly overlaps grid, set a shared mem bitmask,
-	//scan the bitmask and then do a scatter operation to create the array that will be passed to the grid point setting function
-
-	unsigned tIndex = ((threadIdx.x*BLOCKDIM) + threadIdx.y)*BLOCKDIM+threadIdx.z;
+	unsigned tIndex = ((threadIdx.z*BLOCKDIM) + threadIdx.y)*BLOCKDIM+threadIdx.x;
 
 	//there may be more than THREADPERBLOCK atoms, in which case we have to chunk them
 	for(unsigned atomoffset = 0; atomoffset < n; atomoffset += THREADSPERBLOCK)
 	{
+		//first parallelize over atoms to figure out if they might overlap this block
 		unsigned aindex = atomoffset+tIndex;
 		if(aindex < n)
 			atomMask[tIndex] = atomOverlapsBlock(aindex, origin, resolution, coords, radii, gridindex, rmult);
@@ -265,24 +263,21 @@ template<bool Binary> __global__ void gpu_grid_set(float3 origin, int dim, float
 			atomMask[tIndex] = 0;
 
 		__syncthreads();
-
-		uint *scanresult = scan(tIndex); //implicitly uses atomMask and scanOutput
-		//sharedMemExclusiveScan(tIndex, atomMask, scanOutput, scanScratch, THREADSPERBLOCK);
-		__syncthreads();
-
-		unsigned nAtoms = scanresult[THREADSPERBLOCK-1] + atomMask[THREADSPERBLOCK-1];
-
-		assert(scanValid(tIndex,scanresult));
 		
-			
-		//printf("%d %d %d\n",nAtoms, min(THREADSPERBLOCK,n-atomoffset),n);
-		//do scatter
+		//scan the mask to get just relevant indices
+		sharedMemExclusiveScan(tIndex, atomMask, scanOutput);
+		
+		__syncthreads();
+		//assert(scanValid(tIndex,scanresult));
+		
+		//do scatter (stream compaction)
 		if(atomMask[tIndex])
 		{
-			atomIndices[scanresult[tIndex]] = tIndex+atomoffset;
+			atomIndices[scanOutput[tIndex]] = tIndex+atomoffset;
 		}
 		__syncthreads();
 
+		unsigned nAtoms = scanOutput[THREADSPERBLOCK-1] + atomMask[THREADSPERBLOCK-1];
 		//atomIndex is now a list of nAtoms atom indices
 		set_atoms<Binary>(origin, dim, resolution, rmult, nAtoms, coords, gridindex, radii, grids);
 		__syncthreads();//everyone needs to finish before we muck with atomIndices again
@@ -290,7 +285,8 @@ template<bool Binary> __global__ void gpu_grid_set(float3 origin, int dim, float
 }
 
 void NNGridder::setAtomsGPU(unsigned natoms,float3 *coords,short *gridindex,
-		float *radii,unsigned ngrids,float *grids){
+		float *radii,unsigned ngrids,float *grids)
+{
 	//each thread is responsible for a grid point location and will handle all atom types
 	//each block is 8x8x8=512 threads
 	float3 origin = make_float3(dims[0].begin, dims[1].begin, dims[2].begin);
@@ -302,10 +298,11 @@ void NNGridder::setAtomsGPU(unsigned natoms,float3 *coords,short *gridindex,
 	CUDA_CHECK(cudaMemset(grids, 0, ngrids * dim * dim * dim * sizeof(float)));	//TODO: see if faster to do in kernel
 	if(binary){
 		gpu_grid_set<true><<<blocks,threads>>>(origin, dim, resolution, 1.0, natoms, coords, gridindex, radii, grids);
-		CUDA_CHECK (cudaPeekAtLastError() );}
-		else
-		{
-			gpu_grid_set<false><<<blocks,threads>>>(origin, dim, resolution, radiusmultiple, natoms, coords, gridindex, radii, grids);
-			CUDA_CHECK(cudaPeekAtLastError() );
-		}
+		CUDA_CHECK (cudaPeekAtLastError() );
 	}
+	else
+	{
+		gpu_grid_set<false><<<blocks,threads>>>(origin, dim, resolution, radiusmultiple, natoms, coords, gridindex, radii, grids);
+		CUDA_CHECK(cudaPeekAtLastError() );
+	}
+}
