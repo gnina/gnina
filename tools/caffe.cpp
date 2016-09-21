b0964f765ec77cd006bb0eb134243a5a783a474e
David Koes <dkoes@pitt.edu>
2016-09-20 22:52:37 -0400

    work with default gup

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -200,7 +200,10 @@ int train() {
           FLAGS_gpu = "" +
               boost::lexical_cast<string>(solver_param.device_id());
       } else {  // Set default GPU if unspecified
-          FLAGS_gpu = "" + boost::lexical_cast<string>(-1);
+	  int device = -1;
+	  cudaGetDevice(&device);
+	  if(device >= 0)
+          	FLAGS_gpu = "" + boost::lexical_cast<string>(device);
       }
   }
 
