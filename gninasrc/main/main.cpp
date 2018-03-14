45db3d9d84837521f96bc48279ad2492b409e983
Jsunseri <jss97@pitt.edu>
2018-03-14 01:10:48 -0400

    Need to call model::initialize_gpu for correctly determining code path later

--- gninasrc/main/main.cpp
+++ gninasrc/main/main.cpp
@@ -238,7 +238,7 @@ void do_search(model& m, const boost::optional<model>& ref,
     fl intramolecular_energy = max_fl;
 	fl cnnscore = -1.0;
 	fl rmsd = 0;
-    if (settings.gpu_on && !settings.cnnopts.cnn_scoring)
+    if (settings.gpu_on)
 	    m.initialize_gpu();
 	const vec authentic_v(settings.forcecap, settings.forcecap,
 			settings.forcecap); //small cap restricts initial movement from clash
