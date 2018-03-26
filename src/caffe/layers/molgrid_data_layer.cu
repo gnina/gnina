9c8abe4b4a7598b4736647e71e09b408e18d3393
David Koes <dkoes@pitt.edu>
2018-03-26 01:00:37 -0400

    merge in gpu gradients

--- src/caffe/layers/molgrid_data_layer.cu
+++ src/caffe/layers/molgrid_data_layer.cu
@@ -30,25 +30,35 @@ template <typename Dtype>
 void MolGridDataLayer<Dtype>::setAtomGradientsGPU(GridMaker& gmaker, Dtype
         *diff, unsigned batch_size)  {
 
+  unsigned buffersize = 0;
+  float4* atoms = NULL;
+  short* whichGrid = NULL;
+  float3* gradient = NULL; 
   //launch a kernel for each batch
   for (int item_id = 0; item_id < batch_size; ++item_id) {
     int offset = item_id*example_size;
     //malloc and copy batch data
-    float4* atoms;
-    short* whichGrid;
-    float3* gradient;
-
     mol_transform& transform = batch_transform[item_id];
     int natoms = transform.mol.atoms.size();
-    cudaMalloc(&atoms, sizeof(float4)*natoms);
+    
+    if(natoms > buffersize) {
+        buffersize = natoms;
+        if(atoms) {
+          cudaFree(atoms);
+          cudaFree(whichGrid);
+          cudaFree(gradient);
+        }
+        cudaMalloc(&atoms, sizeof(float4)*natoms);
+        cudaMalloc(&whichGrid, sizeof(short)*transform.mol.whichGrid.size());
+        cudaMalloc(&gradient, sizeof(float3)*transform.mol.gradient.size());
+    }
     cudaMemcpy(atoms, &transform.mol.atoms[0],
             sizeof(float4)*transform.mol.atoms.size(), cudaMemcpyHostToDevice);
-    cudaMalloc(&whichGrid, sizeof(short)*transform.mol.whichGrid.size());
     cudaMemcpy(whichGrid, &transform.mol.whichGrid[0],
             sizeof(short)*transform.mol.whichGrid.size(),
             cudaMemcpyHostToDevice);
-    cudaMalloc(&gradient, sizeof(float3)*transform.mol.gradient.size());
     cudaMemset(gradient, 0, sizeof(float3)*transform.mol.gradient.size());
+    
     gmaker.setCenter(transform.center[0], transform.center[1], transform.center[2]);
 
     quaternion& cpu_q = transform.Q;
@@ -58,7 +68,7 @@ void MolGridDataLayer<Dtype>::setAtomGradientsGPU(GridMaker& gmaker, Dtype
     //diff is batch x channel x X x Y x Z
 	unsigned nfull_blocks = natoms / THREADS_PER_BLOCK;
 	unsigned nthreads_remain = natoms % THREADS_PER_BLOCK;
-    std::cout << "natoms " << natoms << std::endl;
+    //std::cout << "natoms " << natoms << std::endl;
 	boost::timer::cpu_timer time;
     if (nfull_blocks)
         setAtomGradientGPU <<<nfull_blocks, THREADS_PER_BLOCK>>>(gmaker, atoms, 
@@ -71,10 +81,13 @@ void MolGridDataLayer<Dtype>::setAtomGradientsGPU(GridMaker& gmaker, Dtype
                 make_float3(transform.center[0], transform.center[1],
                 transform.center[2]), diff, offset, natoms - nthreads_remain);
     cudaStreamSynchronize(cudaStreamPerThread);
-	std::cout << "GPU grid time " << time.elapsed().wall/1000000000.0 << "\n";
+//std::cout << "GPU grid time " << time.elapsed().wall/1000000000.0 << "\n";
     cudaMemcpy(&transform.mol.gradient[0], gradient,
             sizeof(float3)*transform.mol.gradient.size(),
             cudaMemcpyDeviceToHost);
+  }
+  
+  if(atoms) {
     cudaFree(atoms);
     cudaFree(whichGrid);
     cudaFree(gradient);
