43d722fecb8d803436445de7ac72a36c57a39285
David Koes <dkoes@pitt.edu>
2019-04-25 15:22:19 -0400

    tests refactor

--- gninasrc/test/test_gpucode.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_GPUCODE_H
-#define TEST_GPUCODE_H
-
-void test_interaction_energy();
-void test_eval_intra();
-
-#endif
--- gninasrc/test/test_tree.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_TREE_H
-#define TEST_TREE_H
-
-void test_set_conf();
-void test_derivative();
-
-#endif
