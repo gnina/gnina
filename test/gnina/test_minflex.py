fb17042adfa500c2b1b3f4d41424c0f6c038ed09
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-16 13:53:07 +0100

    enable flexible minimisation for test

--- test/gnina/test_minflex.py
+++ test/gnina/test_minflex.py
@@ -7,6 +7,7 @@ gnina = sys.argv[1]  # path to gnina executable
 
 subprocess.check_call('%s  -r data/rec.pdb -l data/lig.mol2 \
     --cnn_scoring --minimize \
+    --flexdist 3 --flexdist_ligand data/lig.mol2 \
     -o lig-min.pdb --out_flex flex.pdb \
     --gpu'
     %(gnina), shell=True)
