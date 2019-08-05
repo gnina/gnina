7d9153eb739c6c1198f80360466744aedb31ba9e
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-05 15:09:59 +0100

    autoremove test output

--- test/gnina/test_flexmin.py
+++ test/gnina/test_flexmin.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 import subprocess
 
 gnina = sys.argv[1]  # path to gnina executable
 
-subprocess.check_call('%s  -r data/1w4o_rec.pdb -l data/1w4o_lig.mol2 \
+def rmout(*files):
+    for file in files:
+        try:
+            os.remove(file)
+        except OSError:
+            pass
+
+outlig="lig-min.pdb"
+outflex="flex-min.pdb"
+
+subprocess.check_call("%s  -r data/1w4o_rec.pdb -l data/1w4o_lig.mol2 \
     --cnn_scoring --minimize \
     --flexdist 3 --flexdist_ligand data/1w4o_lig.mol2 \
-    -o lig-min.pdb --out_flex flex-min.pdb \
-    --gpu'
-    %(gnina), shell=True)
+    -o %s --out_flex %s \
+    --gpu"
+    %(gnina, outlig, outflex), shell=True)
 
-subprocess.check_call('%s  -r data/10gs_rec.pdb -l data/10gs_lig.mol2 \
+rmout(outlig, outflex)
+
+subprocess.check_call("%s  -r data/10gs_rec.pdb -l data/10gs_lig.mol2 \
     --cnn_scoring --minimize \
     --flexdist 3 --flexdist_ligand data/10gs_lig.mol2 \
-    -o lig-min.pdb --out_flex flex-min.pdb \
-    --gpu'
-    %(gnina), shell=True)
+    -o %s --out_flex %s \
+    --gpu"
+    %(gnina, outlig, outflex), shell=True)
+
+rmout(outlig, outflex)
