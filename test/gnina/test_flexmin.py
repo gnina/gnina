40d57b4b2d0e1b810aee79b01766132b3d3c3808
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-05 15:47:01 +0100

    added check in test

--- test/gnina/test_flexmin.py
+++ test/gnina/test_flexmin.py
@@ -3,6 +3,10 @@
 import sys, os
 import subprocess
 
+import numpy as np
+
+from openbabel import pybel
+
 gnina = sys.argv[1]  # path to gnina executable
 
 def rmout(*files):
@@ -12,17 +16,21 @@ def rmout(*files):
         except OSError:
             pass
 
+def moved(output, reference, refformat):
+    mola = next(pybel.readfile('pdb', output))
+    molb = next(pybel.readfile(refformat, reference))
+
+    for a, b in zip(mola.atoms, molb.atoms):           
+            dist = np.linalg.norm(np.array(a.coords)-np.array(b.coords))
+            if dist > 1e-3:
+                return True
+
+    print(output)
+
+    return False
+
 outlig="lig-min.pdb"
 outflex="flex-min.pdb"
-
-subprocess.check_call("%s  -r data/1w4o_rec.pdb -l data/1w4o_lig.mol2 \
-    --cnn_scoring --minimize \
-    --flexdist 3 --flexdist_ligand data/1w4o_lig.mol2 \
-    -o %s --out_flex %s \
-    --gpu"
-    %(gnina, outlig, outflex), shell=True)
-
-rmout(outlig, outflex)
 
 subprocess.check_call("%s  -r data/10gs_rec.pdb -l data/10gs_lig.mol2 \
     --cnn_scoring --minimize \
@@ -31,4 +39,6 @@ subprocess.check_call("%s  -r data/10gs_rec.pdb -l data/10gs_lig.mol2 \
     --gpu"
     %(gnina, outlig, outflex), shell=True)
 
+assert moved(outlig, "data/10gs_lig.mol2", "mol2")
+assert moved(outflex, "data/10gs_rec_ref.pdb", "pdb")
 rmout(outlig, outflex)
