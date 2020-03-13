ad9ad3e778cc27d173aacb6efb6e849f436b0f86
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2020-03-13 16:57:30 +0000

    add tests for both systems, increase residues for 184l, test flex_max

--- test/gnina/test_flex.py
+++ test/gnina/test_flex.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+# Test docking/minimsation with flexible residues
+
+# Reference selections are checked against the following selection in PyMol
+# (after stripping away all hydrogen atoms):
+#
+# select byres 
+#   (RECEPTOR and not backbone 
+#       and not resname ALA and not resname GLY and not resname PRO
+#   ) within DISTANCE of LIGAND
+
 import sys, os
 import subprocess
 
@@ -42,31 +52,58 @@ def moved(output, reference, refformat):
 
 # Test if side chains moved after minimisation with flexible residues
 
-outlig="lig-min.pdb"
-outflex="flex-min.pdb"
+for system, flexdist in [("10gs", 3.0), ("184l", 3.9)]:
 
-subprocess.check_call("%s -r data/10gs_rec.pdb -l data/10gs_lig.mol2 \
-    --cnn_scoring --minimize \
-    --flexdist 3 --flexdist_ligand data/10gs_lig.mol2 \
-    -o %s --out_flex %s \
-    --gpu"
-    %(gnina, outlig, outflex), shell=True)
+    outlig="lig-min-{system}.pdb".format(system=system)
+    outflex="flex-min-{system}.pdb".format(system=system)
 
-assert moved(outlig, "data/10gs_lig.mol2", "mol2")
-assert moved(outflex, "data/10gs_rec_ref.pdb", "pdb")
-rmout(outlig, outflex)
+    subprocess.check_call("{gnina} -r data/{system}_rec.pdb -l data/{system}_lig.sdf \
+        --cnn_scoring --minimize \
+        --flexdist {flexdist} --flexdist_ligand data/{system}_lig.sdf \
+        -o {outlig} --out_flex {outflex} \
+        --gpu".format(gnina=gnina, outlig=outlig, outflex=outflex, system=system, flexdist=flexdist),
+        shell=True)
+
+    assert moved(outlig, "data/{system}_lig.sdf".format(system=system), "sdf")
+    assert moved(outflex, "data/{system}_rec_ref.pdb".format(system=system), "pdb")
+    rmout(outlig, outflex)
 
 # Test if side chains moved after docking with flexible residues
 
-outlig="lig-dock.pdb"
-outflex="flex-dock.pdb"
+for system, flexdist in [("10gs", 3.0), ("184l", 3.9)]:
 
-subprocess.check_call("%s -r data/184l_rec.pdb -l data/184l_lig.sdf \
-    --autobox_ligand data/184l_lig.sdf --autobox_add 6 \
-    --flexdist 3.5 --flexdist_ligand data/184l_lig.sdf \
-    -o %s --out_flex %s"
-    %(gnina, outlig, outflex), shell=True)
+    outlig="lig-dock-{system}.pdb".format(system=system)
+    outflex="flex-dock-{system}.pdb".format(system=system)
 
-assert moved(outlig, "data/184l_lig.sdf", "sdf")
-assert moved(outflex, "data/184l_rec_ref.pdb", "pdb")
-rmout(outlig, outflex, "gnina.log")
+    subprocess.check_call("{gnina} -r data/{system}_rec.pdb -l data/{system}_lig.sdf \
+        --autobox_ligand data/{system}_lig.sdf --autobox_add 6 \
+        --flexdist {flexdist} --flexdist_ligand data/{system}_lig.sdf \
+        --num_mc_steps 50 \
+        -o {outlig} --out_flex {outflex}".format(gnina=gnina, outlig=outlig, outflex=outflex, system=system, flexdist=flexdist), 
+        shell=True)
+
+    assert moved(outlig, "data/{system}_lig.sdf".format(system=system), "sdf")
+    assert moved(outflex, "data/{system}_rec_ref.pdb".format(system=system), "pdb")
+    rmout(outlig, outflex)
+
+# Test --flex_max selection of closest residues
+# --flexdist 3.9 selects 4 residues for 184l
+for closest in [1, 2, 3]:
+
+    outlig="lig-dock-184l-c{closest}.pdb".format(closest=closest)
+    outflex="flex-dock-184l-c{closest}.pdb".format(closest=closest)
+
+    subprocess.check_call("{gnina} -r data/184l_rec.pdb -l data/184l_lig.sdf \
+        --autobox_ligand data/184l_lig.sdf --autobox_add 6 \
+        --flexdist 3.9 --flexdist_ligand data/184l_lig.sdf \
+        --flex_max {closest} \
+        --num_mc_steps 50 \
+        -o {outlig} --out_flex {outflex}".format(gnina=gnina,
+            outlig=outlig,
+            outflex=outflex,
+            closest=closest),
+        shell=True)
+
+    assert moved(outlig, "data/184l_lig.sdf", "sdf")
+    assert moved(outflex, "data/184l_rec_ref_c{closest}.pdb".format(closest=closest), "pdb")
+    rmout(outlig, outflex)
