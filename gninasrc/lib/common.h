7464e51e19ada82b88c2596ee583d8e10b5011ba
David Koes <dkoes@pitt.edu>
2024-05-03 13:41:45 -0400

    tweak fix

--- gninasrc/lib/common.h
+++ gninasrc/lib/common.h
@@ -365,9 +365,9 @@ __host__ inline void normalize_angle(fl &x) { // subtract or add enough 2*pi's t
     x += 2 * pi;
   }
   if (!(x >= -pi - epsilon_fl && x <= pi + epsilon_fl)) {
-    x = 0;
     std::cerr << "Numerical degeneracy encountered. Check for non-physical inputs. " + std::to_string(x) +
-                          " should be between -pi and pi.";
+                     " should be between -pi and pi.\n";
+    x = 0;
   }
   // in [-pi, pi]
 }
@@ -388,7 +388,7 @@ __device__ inline void normalize_angle(fl &x) {
   }
   if (!(x >= -pi - epsilon_fl && x <= pi + epsilon_fl)) {
     x = 0;
-  }  
+  }
   // TODO: put the assert back?
 }
 #endif
